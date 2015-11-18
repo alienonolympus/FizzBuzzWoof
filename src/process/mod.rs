b8d681ad2c3e1089d1906cce19001b46d5e716d9
Carbon-Labs <alien-on-earth@outlook.com>
2015-11-18 19:14:50 +0800

    Fix Skipped Function
    Changed expression so that process::repeat4multiple() is not skipped.

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,27 +1,33 @@
 fn test4repeat(input:i32) -> bool {
     let mut printed = false;
+
     if input - input % 10 == 30 {
         print!("Fizz ");
         printed = true;
     }
+
     if input % 10 == 3 {
         print!("Fizz ");
         printed = true;
     }
+
     if input - input % 10 == 50 {
         print!("Buzz ");
         printed = true;
     }
+
     if input % 10 == 5 {
         print!("Buzz ");
         printed = true;
     }
+
     if input - input % 10 == 70 {
         print!("Woof ");
         printed = true;
     }
+
     if input % 10 == 7 {
-        print!("YEAH ");
+        print!("Woof ");
         printed = true;
     }
 
@@ -30,24 +36,30 @@ fn test4repeat(input:i32) -> bool {
 
 fn test4multiple(input:i32) -> bool {
     let mut printed = false;
+
     if input % 3 == 0 {
         print!("Fizz ");
         printed = true;
     }
+
     if input % 5 == 0 {
         print!("Buzz ");
         printed = true;
     }
+
     if input % 7 == 0 {
         print!("Woof ");
         printed = true;
     }
 
+
     return printed;
 }
 
 pub fn analyse(input:i32) {
-    if !(test4repeat(input.clone()) || test4multiple(input.clone())) {
+    let evaluate1 = test4repeat(input.clone());
+    let evaluate2 = test4multiple(input.clone());
+    if  !(evaluate1 || evaluate2) {
         print!("{}", input);
     };
     println!("");
